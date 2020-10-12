c6b0b8cfd5279c5a19ed39b0f807f7cb61c14e83
BinbinLiu <37534778+code-roamer@users.noreply.github.com>
2020-10-12 22:21:54 +0800

    Update DataProcessing.py

--- DataProcessing/DataProcessing.py
+++ DataProcessing/DataProcessing.py
@@ -1,4 +1,3 @@
-import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
@@ -14,13 +13,19 @@ for train_label in train_labels:
     index_dict.setdefault(index, [0])
     index_dict[index][0] += 1
 
-label_frame = pd.DataFrame.from_dict(index_dict, orient='index')
+label_frame = pd.DataFrame.from_dict(index_dict, orient='index', columns=["Picture Number"])
 plt.figure()
-sns.histplot(data=label_frame, stat="count", discrete=True)
+sns.histplot(data=label_frame, x="Picture Number", stat="count", discrete=True)
+
+"""For Ten TO Inf"""
 plt.xlim(10, 810)
 plt.ylim(0, 10)
 pic_file = 'Pictures/TenToInf.png'
+
+"""For ZeroToTen.png"""
+# plt.xlim(0, 10)
+# pic_file = 'Pictures/ZeroToTen.png'
+
 plt.savefig(pic_file)
 plt.show()
 print(label_frame.describe(include="all"))
-
